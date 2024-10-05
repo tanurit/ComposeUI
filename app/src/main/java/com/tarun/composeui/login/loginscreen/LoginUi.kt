fbf8b757474a56bdec98f7d284e0051832bb506f
tarun_sharma <tarun.sharma@thinkexam.com>
2024-10-05 13:36:41 +0530

    "LoginUi"

--- app/src/main/java/com/tarun/composeui/login/loginscreen/LoginUi.kt
+++ app/src/main/java/com/tarun/composeui/login/loginscreen/LoginUi.kt
@@ -2,32 +2,59 @@ package com.tarun.composeui.login.loginscreen
 
 import android.widget.TextView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tarun.composeui.R
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
- fun LoginUi() {
-        Image(painter =painterResource(R.drawable.splashview),
-        contentDescription=null,
-        modifier = Modifier.fillMaxSize(),
-        contentScale = ContentScale.Crop)
-       Column {
-           Text(
-               text = "Login",
-               fontSize = 30.sp,
-               color = Color.White,
-               textAlign = TextAlign.Center)
-       }
+fun LoginUi() {
+    var mobileNumber by remember { mutableStateOf("") }
 
+    Image(
+        painter = painterResource(R.drawable.splashview),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+    Column(modifier = Modifier.padding(0.dp,40.dp,0.dp,0.dp)) {
+        Text(
+            "Login",
+            color = Color.White,
+            fontSize=30.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField( value = mobileNumber,
+            onValueChange = { it->
+                mobileNumber=it
+            },
+            label = { Text("Enter Mobile Number")},
+            singleLine = true,
+            keyboardOptions= KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 30.dp, 16.dp, 0.dp))
+    }
 }
